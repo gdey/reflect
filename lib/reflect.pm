bd1ccb50fd9d3ab068c6add5ef8e2576f5a501fc
Bob Kleemann <bob.kleemann@tealium.com>
2013-09-23 16:26:43 -0700

    Prevent control characters in the response.

--- lib/reflect.pm
+++ lib/reflect.pm
@@ -7,7 +7,7 @@ our $VERSION = '0.1';
 any '**' => sub {
     content_type 'text/plain';
     my $value = to_dumper(request());
-    $value =~ s/([[:cntrl:]])/sprintf '\x%02x', ord $1/eg;
+    $value =~ s/([\x00-\x09\x0b-\x1f\x7f])/sprintf '\x%02x', ord $1/eg;
     $value;
 };
 
