150970084ad4555e0051d6a0c7c526fcdaaacdfe
Bob Kleemann <bob.kleemann@tealium.com>
2013-09-23 16:22:45 -0700

    Prevent control characters in the response.

--- lib/reflect.pm
+++ lib/reflect.pm
@@ -1,11 +1,12 @@
 package reflect;
 use Dancer ':syntax';
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
 any '**' => sub {
     content_type 'text/plain';
-    my $value = to_dumper($request);
+    my $value = Dumper($request);
     $value =~ s/([[:cntrl:]])/sprintf '\x%02x', ord $1/eg;
     $value;
 };
