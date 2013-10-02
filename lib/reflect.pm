e873362ae6aba91539107d256daa78dd5ff8b3d0
Bob Kleemann <bob.kleemann@tealium.com>
2013-10-02 11:27:35 -0700

    Added logging to a MongoDB collection.

--- lib/reflect.pm
+++ lib/reflect.pm
@@ -1,12 +1,28 @@
 package reflect;
+
 use Dancer ':syntax';
 use Data::Dumper;
+use MongoDB;
 
 our $VERSION = '0.1';
 
+
 any '**' => sub {
     content_type 'text/plain';
-    Data::Dumper->new([request()] => [qw(*request)])
+    my $request = request();
+
+    # mongodb://reflect:reflect123@ds049198.mongolab.com:49198/reflect-db
+    MongoDB::MongoClient->new(
+        host => 'mongodb://'
+            . 'reflect:reflect123'
+            . '@ds049198.mongolab.com:49198'
+            . '/reflect-db'
+    )
+        ->get_database('reflect-db')
+        ->get_collection('requests')
+        ->insert($request);
+
+    Data::Dumper->new([$request] => [qw(*request)])
         ->Indent(1)
         ->Useqq(1)
         ->Purity(0)
